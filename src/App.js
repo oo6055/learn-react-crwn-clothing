50fb46ccf3f06c708125a98e951ae7601082ce9d
Ori Ofek <oriofek106@gmail.com>
2022-09-18 01:11:19 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import HomePage from './component/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
       <HomePage>
-        
+
       </HomePage>
     </div>
   );
